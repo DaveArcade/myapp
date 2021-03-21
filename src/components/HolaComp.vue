0b28487c50d09453d1178b5a6101c4366fbddee5
DaveArcade <davearcade118@gmail.com>
2021-03-21 12:55:53 -0600

    First commit

---
+++ src/components/HolaComp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="container">
+    <strong>Bienvenido a Book Break</strong>
+    <p>Empecemos</p>
+  </div>
+</template>
+
+<script lang="ts">
+
+export default {
+  name: 'Hola',
+  props: {
+    name: String
+  }
+}
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 30px;
+  line-height: 26px;
+  color: #ffffff;
+}
+
+#container p {
+  font-size: 26px;
+  line-height: 22px;
+  color: #ffffff;
+  margin: 0;
+}
+
+</style>
---
+++ src/views/Tab1.vue
@@ -0,0 +1,68 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Home</ion-title>
+        </ion-toolbar>
+      </ion-header>
+      <div id="bg">
+        <ion-list>
+          <ion-item>
+            <ion-checkbox slot="start"></ion-checkbox>
+            <ion-label>
+              <h3>Recibir notificaciones</h3>
+              <ion-note>de decuentos</ion-note>
+            </ion-label>
+          </ion-item>
+        </ion-list>
+        <HolaComp />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+} from "@ionic/vue";
+import HolaComp from "@/components/HolaComp.vue";
+
+export default {
+  name: "Tab1",
+  components: {
+    HolaComp,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonPage,
+  },
+  setup() {
+    return {
+    };
+  },
+};
+</script>
+<style scoped>
+#bg {
+  background-image: url("../../public/assets/img/bienvenido1.jpg");
+  width: 100%;
+  height: 100%;
+  background-position-x: 20%;
+  background-position-y: 20%;
+}
+.ic {
+  padding-top: 40%;
+  padding-left: 46%;
+}
+</style>
